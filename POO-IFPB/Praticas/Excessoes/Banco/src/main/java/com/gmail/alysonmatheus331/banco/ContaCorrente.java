e5495516589dad33bb1b91154de8893d00a06b41
Alyson Vale <47997158+alysonvale@users.noreply.github.com>
2021-04-26 16:48:08 -0300

    Delete POO-IFPB directory

--- POO-IFPB/Praticas/Excessoes/Banco/src/main/java/com/gmail/alysonmatheus331/banco/ContaCorrente.java
+++
@@ -1,8 +0,0 @@
-package com.gmail.alysonmatheus331.banco;
-
-public class ContaCorrente extends Conta {
-
-    public ContaCorrente(int número, String titular, double deposito_Inicial) {
-        super(número, titular, deposito_Inicial);
-    }
-}
